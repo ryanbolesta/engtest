230b20cd1be5ffc057e909c84365225edeeb2413
ryanbolesta <ryanbolesta@gmail.com>
2020-03-17 18:18:45 -0600

    Improved exception handling in CompositeUserController and added logging

--- src/main/java/fitpay/engtest/controller/CompositeUserController.java
+++ src/main/java/fitpay/engtest/controller/CompositeUserController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import fitpay.engtest.exception.FitPayAPIException;
 import fitpay.engtest.model.CompositeUser;
 import fitpay.engtest.service.CompositeUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +22,8 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class CompositeUserController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(CompositeUserController.class);
+
     @Autowired
     private CompositeUserService compositeUserService;
 
@@ -27,19 +31,28 @@ public class CompositeUserController {
     public ResponseEntity<CompositeUser> getCompositeUsers(@PathVariable String userId,
                                                            @RequestParam(required = false) String deviceState,
                                                            @RequestParam(required = false) String creditCardState) {
+
+        LOGGER.debug("Request received to create composite user for userId={}", userId);
+
         try {
             CompositeUser compositeUser = compositeUserService.getCompositeUser(userId, deviceState, creditCardState);
             return ResponseEntity.ok(compositeUser);
+
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
-                    "Error processing JSON when retrieving Composite User", e);
+            final String errorMessage = "Error processing JSON when retrieving Composite User";
+            LOGGER.error(errorMessage, e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, e);
+
         } catch (FitPayAPIException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
-                    "Error connecting to the FitPay API when retrieving Composite User");
+            final String errorMessage = "Error connecting to FitPay API when retrieving Composite User";
+            LOGGER.error(errorMessage, e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, e);
+
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "Error occurred when retrieving Composite User", e);
+            final String errorMessage = "Error occurred when retrieving Composite User";
+            LOGGER.error(errorMessage, e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, e);
+
         }
     }
 
